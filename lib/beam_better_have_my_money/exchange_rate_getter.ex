9bde6ab911cf5d9abcec6043418cb71f64dde368
Cornelia Kelinske <corneliakelinske@gmail.com>
2022-07-04 15:50:09 -0700

    switch request over to local

--- lib/beam_better_have_my_money/exchange_rate_getter.ex
+++ lib/beam_better_have_my_money/exchange_rate_getter.ex
@@ -2,17 +2,17 @@ defmodule BeamBetterHaveMyMoney.ExchangeRateGetter do
   @moduledoc false
 
   def request_exchange_rate() do
-    HTTPoison.get("https://www.alphavantage.co/query", [],
+    HTTPoison.get("localhost:4001/query", [],
       params: [
         function: "CURRENCY_EXCHANGE_RATE",
         from_currency: "CAD",
-        to_currency: "EUR",
-        apikey: api_key()
+        to_currency: "EUR"
+
       ]
     )
   end
 
-  defp api_key do
-    Application.get_env(:beam_better_have_my_money, :api_key) 
-  end
+  # defp api_key do
+  #   Application.get_env(:beam_better_have_my_money, :api_key)
+  # end
 end
