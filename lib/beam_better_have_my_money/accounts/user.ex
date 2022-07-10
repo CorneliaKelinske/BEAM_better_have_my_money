fd68c4d9b0831947ce2f5b570b2d7bcf0a38c663
Cornelia Kelinske <corneliakelinske@gmail.com>
2022-07-10 07:31:51 -0700

    improve schema modules

--- lib/beam_better_have_my_money/accounts/user.ex
+++ lib/beam_better_have_my_money/accounts/user.ex
@@ -1,18 +1,31 @@
 defmodule BEAMBetterHaveMyMoney.Accounts.User do
+  @moduledoc false
   use Ecto.Schema
   import Ecto.Changeset
+  alias BEAMBetterHaveMyMoney.Accounts.Wallet
+
+  @type t :: %__MODULE__{
+    id: pos_integer | nil,
+    name: String.t() | nil,
+    email: String.t() | nil
+  }
 
   schema "users" do
     field :email, :string
     field :name, :string
 
+    has_many :wallets, Wallet
+
     timestamps()
   end
 
-  @doc false
+  @required_params [:name, :email]
+
+  @spec changeset(t, map) :: Ecto.Changeset.t()
   def changeset(user, attrs) do
     user
-    |> cast(attrs, [:name, :email])
-    |> validate_required([:name, :email])
+    |> cast(attrs, @required_params)
+    |> validate_required(@required_params)
+    |> unique_constraint(:email)
   end
 end
--- lib/beam_better_have_my_money/accounts/wallet.ex
+++ lib/beam_better_have_my_money/accounts/wallet.ex
@@ -1,19 +1,33 @@
 defmodule BEAMBetterHaveMyMoney.Accounts.Wallet do
+  @moduledoc false
   use Ecto.Schema
   import Ecto.Changeset
+  alias BEAMBetterHaveMyMoney.Accounts.User
+
+  @type t :: %__MODULE__{
+    id: pos_integer | nil,
+    cent_amount: integer | nil,
+    currency: String.t() | nil,
+    user_id: pos_integer | nil
+  }
+
 
   schema "wallets" do
     field :cent_amount, :integer
     field :currency, :string
-    field :user_id, :id
+    belongs_to :user, User
 
     timestamps()
   end
 
-  @doc false
+  @required_params [:currency, :cent_amount]
+
+  @spec changeset(t, map) :: Ecto.Changeset.t()
   def changeset(wallet, attrs) do
     wallet
-    |> cast(attrs, [:currency, :cent_amount])
-    |> validate_required([:currency, :cent_amount])
+    |> cast(attrs, @required_params)
+    |> validate_required(@required_params)
+    |> unique_constraint([:currency, :user_id])
+
   end
 end
